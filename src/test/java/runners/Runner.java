762c8b609ebd6258283799137436eb24491cd126
Your Name <you@example.com>
2024-06-12 13:03:16 +0300

    1

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,20 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {
+                "pretty",//prints colored logs to the console
+                "html:target/reports/default-cucumber-reports.html"
+        },
+        features = "src/test/resources/features",
+        glue = {"stepdefinitions", "base_urls"},//Java kodlarının çalışacağı package'lar.
+        tags = "@End2End",
+        dryRun = false// dryRun = true yapıldığında test çalışmaz sadece eksik step definition methodları tespit edilir.
+
+)
+public class Runner {}
---
+++ src/test/java/utilities/Authentication.java
@@ -0,0 +1,41 @@
+package utilities;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static stepdefinitions.CreateUserBySeleniumStepDefinitions.email;
+
+public class Authentication {
+
+
+    public static String generateToken() {
+
+        Map<String, String> payload = new HashMap<>();
+
+        if (email != null) {//Eğer selenium ile user oluşturulursa email null kalmaz.
+            payload.put("email", email);
+            payload.put("password", "Password.123");
+        } else {//User oluşturulmadıysa clarusway kullanıcısı ile token alınacak
+            payload.put("email", "clarusway2024@hotmail.com");
+            payload.put("password", "Clarusway.123");
+        }
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(payload)
+                .post("https://thinking-tester-contact-list.herokuapp.com/users/login");
+        //response.prettyPrint();
+
+        try {//Default olarak alınan alınamadığı taktirde exceptiın'ı handle etmek için kullanıyoruz.
+            return response.jsonPath().getString("token");
+        } catch (Exception e) {
+            System.err.println("Token alınamadı");
+            return "";
+        }
+
+    }
+}
